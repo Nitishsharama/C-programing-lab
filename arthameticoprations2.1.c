b34dd5ed78c28a27ce3ed7ff7ed220fbf2e5a338
Nitishsharama <147177384+Nitishsharama@users.noreply.github.com>
2023-10-09 22:27:51 +0530

    arthametic opration like add,sub,mul and div2.1

---
+++ arthameticoprations2.1.c
@@ -0,0 +1,16 @@
+//programe for arthametic oprations.
+#include <stdio.h>
+int main (){
+    int a,b,c;
+    printf("enter two no's");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("sum=%d\n",c);
+    c=a-b;
+    printf("sub=%d\n",c);
+    c=a*b;
+    printf("mul=%d\n",c);
+    c=a/b;
+    printf("div=%d\n",c);
+    return 0;
+}
